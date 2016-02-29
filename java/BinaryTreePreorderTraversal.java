ffd09c3cc660e5d815b4e4ad591224c7a05327ec
Chen Huang <alan.tolearn@gmail.com>
2016-02-29 17:40:14 -0500

    BT & Divide Conquer: BT preorder traversal
    - traverse

--- java/BinaryTreePreorderTraversal.java
+++ java/BinaryTreePreorderTraversal.java
@@ -9,6 +9,32 @@
  *     }
  * }
  */
+// traverse
+public class BinaryTreePreorderTraversal {
+  /**
+   * @param root: The root of binary tree.
+   * @return: Preorder in ArrayList which contains node values.
+   */
+  public ArrayList<Integer> preorderTraversal(TreeNode root) {
+    ArrayList<Integer> result = new ArrayList<Integer>();
+
+    traverse(root, result);
+
+    return result;
+  }
+
+  private void traverse(TreeNode root, ArrayList<Integer> result) {
+    if (root == null) {
+      return;
+    }
+
+    result.add(root.val);
+    traverse(root.left, result);
+    traverse(root.right, result);
+  }
+}
+
+// divide and conquer
 public class BinaryTreePreorderTraversal {
   /**
    * @param root: The root of binary tree.
